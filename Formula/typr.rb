66902faff27ced5c480e169d6cb640ffff57f6aa
Willyam Arcand <willyam.arcand@wealthsimple.com>
2025-08-05 11:27:38 -0400

    add proper versioning

--- Formula/typr.rb
+++ Formula/typr.rb
@@ -1,8 +1,8 @@
 class Typr < Formula
   desc 'Terminal-based typing speed test with real-time feedback'
   homepage 'https://github.com/willyamjarcand/typr'
-  url 'https://github.com/willyamjarcand/typr.git', branch: 'main'
-  version '0.1.0'
+  url 'https://github.com/willyamjarcand/typr/archive/v0.1.0.tar.gz'
+  sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
   head 'https://github.com/willyamjarcand/typr.git'
   license 'MIT'
 
